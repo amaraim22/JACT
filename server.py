ca51273586546fe71cd4f8dee8cbfd3c1a32c6aa
amaraim22 <51249187+amaraim22@users.noreply.github.com>
2021-01-18 20:20:30 -0500

    1/18

--- server.py
+++ server.py
@@ -12,5 +12,35 @@ def return_about():
     return render_template("about.html")
 
 @app.route('/portfolio')
-def return_portfolio():
-    return render_template("portfolio.html")
+def return_users():
+    with open('data/users.csv') as file:
+        data = csv.reader(file, delimiter=',')
+        first_line = True
+        users = []
+        for row in data:
+            if not first_line:
+                users.append({
+                "fname": row[0],
+                "lname": row[1],
+                "bio": row[2]
+                })
+            else:
+                first_line = False
+    return render_template("portfolio.html", users=users)
+
+@app.route('/newUser', methods=["GET", "POST"])
+def submit_form():
+    if request.method == "GET":
+        return redirect(url_for('newUser'))
+    elif request.method == "POST":
+        userdata = dict(request.form)
+        fname = userdata["fname"]
+        lname = userdata["lname"]
+        bio = userdata["bio"]
+        if( len(fname) < 1 or len(lname) < 1 or len(bio) < 1 ):
+            return render_template("portfolio.html", status='Please resubmit with valid information.')
+        else:
+            with open('data/users.csv', mode='a', newline='') as file:
+                data = csv.writer(file)
+                data.writerow([fname, lname, bio])           
+            return render_template("portfolio.html")
